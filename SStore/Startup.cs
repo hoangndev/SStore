dfd8ec4ea5448f67302e41c4340730eee7c23764
Nguyen Hoang <hoangngcd18786@fpt.edu.vn>
2022-08-13 08:00:34 +0700

    Add project files.

---
+++ SStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SStore.Startup))]
+namespace SStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
