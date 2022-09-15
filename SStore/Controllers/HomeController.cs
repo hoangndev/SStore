6d323c7b5c1c2864ec703edcf2ead53bba979030
Nguyen Hoang <hoangngcd18786@fpt.edu.vn>
2022-09-16 03:07:03 +0700

    Update homepage

--- SStore/Controllers/HomeController.cs
+++ SStore/Controllers/HomeController.cs
@@ -1,16 +1,40 @@
 ﻿using System;
+using System.Data;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SStore.Models;
 
 namespace SStore.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        // GET: /Product
+        public PartialViewResult NewProductListPartial()
+        {
+            var newProductList = db.Products.Include(p => p.productBrand).Include(p => p.ProductCategory).Where(p => p.BrandId.Equals(1)).ToList();
+            return PartialView(newProductList);
+        }
+
+        public PartialViewResult HotProductListPartial()
+        {
+            var hotProductList = db.Products.Include(p => p.productBrand).Include(p => p.ProductCategory).Where(p => p.BrandId.Equals(2)).ToList();
+            return PartialView(hotProductList);
+        }
+        public PartialViewResult FashionProductListPartial()
+        {
+            var fashionProductList = db.Products.Include(p => p.productBrand).Include(p => p.ProductCategory).Where(p => p.BrandId.Equals(1)).OrderByDescending(p => p.Id).ToList();
+            return PartialView(fashionProductList);
         }
 
         public ActionResult About()
