277e0941533b2c2ba5316ec6723c56d4700e0589
Nguyen Hoang <hoangngcd18786@fpt.edu.vn>
2022-09-17 01:58:38 +0700

    Add Feedback Controller and Views

---
+++ SStore/Areas/Admin/Controllers/FeedbackController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SStore.Models;
+
+namespace SStore.Areas.Admin.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Feedback
+        public ActionResult Index()
+        {
+            return View(db.Feedbacks.ToList());
+        }
+
+        // GET: Admin/Feedback/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Feedback feedback = db.Feedbacks.Find(id);
+            if (feedback == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedback);
+        }
+
+        // GET: Admin/Feedback/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Feedback/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Description,Email,PhoneNumber")] Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Feedbacks.Add(feedback);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(feedback);
+        }
+
+        // GET: Admin/Feedback/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Feedback feedback = db.Feedbacks.Find(id);
+            if (feedback == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedback);
+        }
+
+        // POST: Admin/Feedback/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Feedback feedback = db.Feedbacks.Find(id);
+            db.Feedbacks.Remove(feedback);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
